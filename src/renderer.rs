ee06031c38fc5a596529b3d8c79a0c9469a05a82
Ádám Olivér Dömötör <domotoradam01@gmail.com>
2025-07-12 22:06:29 +0300

    Set up WGPU State

--- src/renderer.rs
+++ src/renderer.rs
@@ -10,7 +10,40 @@ pub struct State {
 
 impl State {
 
-    async fn new() -> anyhow::Result<State> {
-        
+    async fn new(window: Arc<Window>) -> anyhow::Result<State> {
+        let instance = wgpu::Instance::new(wgpu::Backends::all());
+        let surface = instance.create_surface(window.clone()).unwrap();
+
+        let adapter = instance
+            .request_adapter(&wgpu::RequestAdapterOptions {
+                power_preference: wgpu::PowerPreference::default(),
+                compatible_surface: Some(&surface),
+                force_fallback_adapter: false,
+            }).await?;
+
+        let (device, queue) = adapter
+            .request_device(&Default::default(), None)
+            .await?;
+
+        let surface_format = surface.get_capabilities(&adapter).formats[0];
+
+        let config = wgpu::SurfaceConfiguration {
+            usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+            format: surface_format,
+            width: window.inner_size().width,
+            height: window.inner_size().height,
+            present_mode: wgpu::PresentMode::Fifo,
+            alpha_mode: wgpu::CompositeAlphaMode::Auto,
+            view_formats: vec![],
+            desired_maximum_frame_latency: 2,
+        };
+
+        Ok(Self {
+            surface,
+            device,
+            queue,
+            config,
+            window,
+        })
     }
 }
